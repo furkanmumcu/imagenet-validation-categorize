39b5e56bd92bbcfc5806d228a2f331102d4ea238
Furkan Mumcu <furkanmumcu2@gmail.com>
2022-07-17 00:05:56 -0400

    update

--- categorize.py
+++ categorize.py
@@ -1,10 +1,10 @@
-import os
 import utils
+import shutil
 
 index_file = 'imagenet_class_index.json'
-val_classes_dir = 'ILSVRC2012_validation_ground_truth.txt'
-target_folder = 'imgnet'
-val_dir = 'test'
+val_classes_dir = 'val_truth_caffe.txt'
+target_folder = 'imagenet_val_cats'
+val_dir = 'imagenet_val'
 
 # get imagenet index
 index = utils.get_imagenet_index(index_file)
@@ -20,13 +20,17 @@ utils.create_subfolders(folders, target_folder)
 val_classes = utils.get_validation_classes(val_classes_dir)
 print(len(val_classes))
 
-# move val images to corresponding folders
-val_files = utils.list_file_names(val_dir)
 
 for i in range(len(val_classes)):
 	val_class = val_classes[i]
-	subfolder_name = index[str(val_class)]
-	pic_file_location = val_dir + '/' + val_files[i]
+	val_class = val_class.split()
+	val_class_file_name = val_class[0]
+	val_class_index = val_class[1]
+
+	pic_file_location = val_dir + '/' + val_class_file_name
+
+	subfolder_name = index[str(val_class_index)][0]
 	target_subfolder = target_folder + '/' + subfolder_name
+
 	print(pic_file_location + " " + target_subfolder)
-	#os.replace(pic_file_location, target_subfolder)
+	shutil.move(pic_file_location, target_subfolder)
